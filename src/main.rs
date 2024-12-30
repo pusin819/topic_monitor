4b957c70ce11fe0e80f7a7d08d459ead5aaa1de0
pusin819 <shibata0819haru@gmail.com>
2024-12-30 18:17:24 +0900

    complete receive

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,10 @@
 #![allow(rustdoc::missing_crate_level_docs)] // Example
 
 use eframe::egui;
-use futures::{executor::LocalPool, future, stream::StreamExt, task::LocalSpawnExt};
+use futures::stream::StreamExt;
 use r2r::{Context, Node, QosProfile};
-use serde_json::Value;
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
-use tokio::sync::mpsc;
-
-struct SubscribeType {
-    node: Arc<Mutex<Node>>,
-    topic: String,
-    message_type: String,
-}
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -23,20 +15,13 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let options = eframe::NativeOptions {
         ..Default::default()
     };
-    let (tx, mut rx) = mpsc::channel::<(String, String)>(10);
 
     let ctx = Context::create().unwrap();
-    let mut node = Node::create(ctx, "sub_node", "namespace").unwrap();
+    let node = Node::create(ctx, "sub_node", "namespace").unwrap();
     let arc_node = Arc::new(Mutex::new(node));
 
-    let sub_type = Arc::new(Mutex::new(SubscribeType {
-        node: arc_node.clone(),
-        topic: "/chatter".to_string(),
-        message_type: "std_msgs/msg/String".to_string(),
-    }));
-
     let n = arc_node.clone();
-    tokio::task::spawn(async move { subscriber(n).await.unwrap() });
+    // tokio::task::spawn(async move { subscriber(n).await.unwrap() });
 
     let handle = tokio::task::spawn_blocking(move || loop {
         {
@@ -51,7 +36,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let _ = eframe::run_native(
         "My Monitoring App",
         options,
-        Box::new(|_cc| Ok(Box::new(MyApp::default_with_channel(sub_type.clone())))),
+        Box::new(|_cc| Ok(Box::new(MyApp::default_with_channel(n)))),
     );
 
     handle.await.unwrap();
@@ -81,14 +66,16 @@ async fn subscriber(arc_node: Arc<Mutex<r2r::Node>>) -> Result<(), r2r::Error> {
 
 struct MyApp {
     node_map: HashMap<String, Vec<String>>,
-    sub: Arc<Mutex<SubscribeType>>,
+    node: Arc<Mutex<Node>>,
+    sub_handle: Option<tokio::task::JoinHandle<()>>,
 }
 
 impl MyApp {
-    fn default_with_channel(sub: Arc<Mutex<SubscribeType>>) -> Self {
+    fn default_with_channel(node: Arc<Mutex<Node>>) -> Self {
         Self {
+            node,
             node_map: HashMap::new(),
-            sub,
+            sub_handle: None,
         }
     }
 }
@@ -107,15 +94,7 @@ impl eframe::App for MyApp {
                 .on_hover_text("Refresh topic list")
                 .clicked()
             {
-                match self
-                    .sub
-                    .lock()
-                    .unwrap()
-                    .node
-                    .lock()
-                    .unwrap()
-                    .get_topic_names_and_types()
-                {
+                match self.node.lock().unwrap().get_topic_names_and_types() {
                     Ok(topics) => {
                         self.node_map = topics;
                     }
@@ -130,27 +109,17 @@ impl eframe::App for MyApp {
                 .on_hover_text("Subscribe to /joy")
                 .clicked()
             {
-                /*
-                                let sub = self
-                                    .node
-                                    .subscribe_untyped("/chatter", "std_msgs/msg/String", QosProfile::default())
-                                    .unwrap();
+                match self.sub_handle.take() {
+                    Some(handle) => {
+                        handle.abort();
+                    }
+                    None => {}
+                }
+                let n = self.node.clone();
 
-                                let _ = self.spawner.spawn_local(async move {
-                                    let _ = sub
-                                        .for_each(|msg| {
-                                            match msg {
-                                                Ok(msg) => match serde_json::to_string_pretty(&msg) {
-                                                    Ok(json) => println!("Received message: {}\n---\n", json),
-                                                    Err(err) => eprintln!("Failed to serialize message: {}", err),
-                                                },
-                                                Err(err) => eprintln!("Failed to receive message: {}", err),
-                                            }
-                                            futures::future::ready(())
-                                        })
-                                        .await;
-                                });
-                */
+                self.sub_handle = Some(tokio::task::spawn(
+                    async move { subscriber(n).await.unwrap() },
+                ));
             }
         });
     }
