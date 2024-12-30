2a510e57cc88a5d305c09a7b8fc84903e69c09a4
pusin819 <shibata0819haru@gmail.com>
2024-12-30 21:49:08 +0900

    add radio button

--- src/main.rs
+++ src/main.rs
@@ -43,21 +43,34 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn subscriber(arc_node: Arc<Mutex<r2r::Node>>) -> Result<(), r2r::Error> {
+async fn subscriber(
+    arc_node: Arc<Mutex<r2r::Node>>,
+    sender_channel: Arc<Mutex<tokio::sync::watch::Sender<String>>>,
+    ctx: &egui::Context,
+    topic: &str,
+    msg_type: &str,
+) -> Result<(), r2r::Error> {
     let sub = arc_node
         .lock()
         .unwrap()
-        .subscribe_untyped("/chatter", "std_msgs/msg/String", QosProfile::default())
+        .subscribe_untyped(topic, msg_type, QosProfile::default())
         .unwrap();
 
     let _ = sub
-        .for_each(|msg| async move {
-            match msg {
-                Ok(msg) => match serde_json::to_string_pretty(&msg) {
-                    Ok(json) => println!("Received message: {}\n---\n", json),
-                    Err(err) => eprintln!("Failed to serialize message: {}", err),
-                },
-                Err(err) => eprintln!("Failed to receive message: {}", err),
+        .for_each(|msg| {
+            let s = sender_channel.clone();
+            async move {
+                match msg {
+                    Ok(msg) => match serde_json::to_string_pretty(&msg) {
+                        Ok(json) => {
+                            println!("received msg \n{}\n---\n", json);
+                            s.lock().unwrap().send(json).unwrap();
+                            ctx.request_repaint();
+                        }
+                        Err(err) => eprintln!("Failed to serialize message: {}", err),
+                    },
+                    Err(err) => eprintln!("Failed to receive message: {}", err),
+                }
             }
         })
         .await;
@@ -68,25 +81,64 @@ struct MyApp {
     node_map: HashMap<String, Vec<String>>,
     node: Arc<Mutex<Node>>,
     sub_handle: Option<tokio::task::JoinHandle<()>>,
+    sender_channel: Arc<Mutex<tokio::sync::watch::Sender<String>>>,
+    receiver_channel: tokio::sync::watch::Receiver<String>,
+    clicked_topic: String,
+    clicked_type: String,
 }
 
 impl MyApp {
     fn default_with_channel(node: Arc<Mutex<Node>>) -> Self {
+        let (sender, receiver) = tokio::sync::watch::channel("no data received".to_string());
         Self {
             node,
+            sender_channel: Arc::new(Mutex::new(sender)),
+            receiver_channel: receiver,
             node_map: HashMap::new(),
             sub_handle: None,
+            clicked_topic: "".to_string(),
+            clicked_type: "".to_string(),
         }
     }
 }
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Topic list");
+        egui::SidePanel::left("Topic List").show(ctx, |ui| {
+            ui.heading("Topic List");
+
             for (name, types) in &self.node_map {
-                ui.label(name);
-                ui.label(types.join(", "));
+                if ui
+                    .radio(self.clicked_topic == *name, name)
+                    .on_hover_text(format!("{:?}", types))
+                    .clicked()
+                {
+                    self.clicked_topic = name.to_string();
+                    self.clicked_type = types[0].to_string();
+
+                    // Delete the previous subscriber
+                    self.sender_channel
+                        .lock()
+                        .unwrap()
+                        .send("".to_string())
+                        .unwrap();
+
+                    match self.sub_handle.take() {
+                        Some(handle) => {
+                            handle.abort();
+                        }
+                        None => {}
+                    }
+                    let n = self.node.clone();
+                    let s = self.sender_channel.clone();
+                    let c = ctx.clone();
+                    let topic = self.clicked_topic.clone();
+                    let msg_type = self.clicked_type.clone();
+
+                    self.sub_handle = Some(tokio::task::spawn(async move {
+                        subscriber(n, s, &c, &topic, &msg_type).await.unwrap()
+                    }));
+                }
             }
 
             if ui
@@ -102,25 +154,12 @@ impl eframe::App for MyApp {
                         eprintln!("Failed to refresh topic list: {:?}", err);
                     }
                 }
-            }
 
-            if ui
-                .button("Add Subscriber")
-                .on_hover_text("Subscribe to /joy")
-                .clicked()
-            {
-                match self.sub_handle.take() {
-                    Some(handle) => {
-                        handle.abort();
-                    }
-                    None => {}
-                }
-                let n = self.node.clone();
-
-                self.sub_handle = Some(tokio::task::spawn(
-                    async move { subscriber(n).await.unwrap() },
-                ));
+                ctx.request_repaint();
             }
+        });
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.label(self.receiver_channel.borrow_and_update().to_string());
         });
     }
 }
